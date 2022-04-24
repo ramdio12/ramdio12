2a2a7373656d0d6a3b23aed759927d1175092052
radmdio12 <luib.diomar@gmail.com>
2022-04-24 21:32:51 +0800

    AdminController added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin',[AdminController::class,'adminDashboard']);
+
 
 Route::middleware([
     'auth:sanctum',
